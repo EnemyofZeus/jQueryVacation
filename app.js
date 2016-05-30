b6c323c61242c7480b9887d9b421fd55d0940113
Kyle Hamburg <kylelhamburg@gmail.com>
2016-05-29 22:39:44 -0600

    Changed images, tweaked menu settings

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ $(document).ready(function(){
   $(".menu").on("click", function(){
   //  $(".container").fadeOut(10);
   //  $(".container").show(10);
-  $(".hidden").show();
+  $(".hidden").animate({width:'toggle'},350);
   //$(".scam").animate({left: '250px'});
 
   //  $(".container").animate({"opacity":".25"},2500).animate({"opacity":"1"},2500);
   })
+  $('#arrow-left').on("click", function(){
+    $('.linkbox').cycle({
+      fx:'scrollLeft',
+      next: '.linkbox',
+      timeout: 0
+    });
+  });
   $(function (){
     function runIt(){
       $(".container").animate({"opacity":".25"},1500).animate({"opacity":"1"},1500);
@@ -15,6 +22,9 @@ $(document).ready(function(){
       runIt();
     }
   //  runIt();
+  });
+  $(".scam").hover(function() {
+    $()
   });
 
 
